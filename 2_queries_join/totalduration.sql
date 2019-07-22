0d80ea807d36714f55ea296cb1939a595fefbcb7
Scott Appleton <Scottappleton09@gmail.com>
2019-07-22 22:37:09 +0000

    added two more queries

---
+++ 2_queries_join/totalduration.sql
@@ -0,0 +1,5 @@
+SELECT SUM(assignment_submissions.duration)
+FROM assignment_submissions
+INNER JOIN students
+ON assignment_submissions.student_id = students.id
+WHERE students.name = 'Ibrahim Schimmel';
